382c262b05b7b6bc58ca83b441014bf814f24eed
Mike Lim <mike@mjlim.net>
2016-03-12 17:07:39 -0800

    improvements to setup script

--- setupwin.ps1
+++ setupwin.ps1
@@ -2,13 +2,31 @@
 
 if (!$env:HOME) # emacs looks here to pull in the spacemacs config.
 {
-    $env:HOME = $env:USERPROFILE
+    echo "Setting HOME environment variable to $env:USERPROFILE"
+    [Environment]::SetEnvironmentVariable("HOME", $env:USERPROFILE, "Machine")
+}
+else
+{
+    echo "HOME environment variable already exists and is $env:HOME"
 }
 
 if (Get-Command "git.exe" -ErrorAction SilentlyContinue)
 {
     echo "This machine has git."
-
+    if (Test-path $env:USERPROFILE/.spacemacs.d/)
+    {
+        echo ".spacemacs.d exists. updating it..."
+        pushd $env:USERPROFILE/.spacemacs.d
+        git pull origin master
+        popd
+    }
+    else
+    {
+        echo ".spacemacs.d doesn't exist. cloning from my github"
+        pushd $env:USERPROFILE
+        git clone git@github.com:mjlim/.spacemacs.d.git
+        popd
+    }
 }
 else
 {
@@ -28,7 +46,6 @@ else
     echo "This machine doesn't have emacs installed; getting it & installing to c:\emacs"
     wget http://d.mjlim.net/~mikel/emacs.zip -outfile $env:temp\emacs.zip
     Expand-Archive $env:temp\emacs.zip -dest c:\
-
 }
 
 # check regkey to turn off scaling
@@ -36,7 +53,6 @@ $layers = Get-Item "hklm:\Software\Microsoft\Windows NT\CurrentVersion\AppCompat
 if ($layers.GetValueNames().Contains("c:\emacs\bin\emacs.exe"))
 {
     echo "This machine has the highdpi aware registry flags set on the emacs exes."
-
 }
 else
 {
@@ -45,4 +61,6 @@ else
     $layers | New-ItemProperty -name "c:\emacs\bin\runemacs.exe" -value "~ HIGHDPIAWARE"
     echo "if that failed, try again as admin."
 }
+
+
 
