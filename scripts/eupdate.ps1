493f4156c8668e7649a274e62ccc4e35544d787f
Mike Lim <mike@mjlim.net>
2016-03-30 21:11:26 -0700

    don't start emacs at the end of eupdate (it's kind of annoying)

--- scripts/eupdate.ps1
+++ scripts/eupdate.ps1
@@ -197,9 +197,9 @@ else
 
 
 # finally start emacs to update packages.
-if (Test-path $env:USERPROFILE/.spacemacs.d/scripts/e.bat)
-{
-    # load runemacs.exe since for some reason that makes windows start honoring the dpi flag regkeys???
-    Start-Process -FilePath "c:\emacs\bin\runemacs.exe"
-    # & $env:USERPROFILE/.spacemacs.d/scripts/e.bat
-}
+# if (Test-path $env:USERPROFILE/.spacemacs.d/scripts/e.bat)
+# {
+#     # load runemacs.exe since for some reason that makes windows start honoring the dpi flag regkeys???
+#     Start-Process -FilePath "c:\emacs\bin\runemacs.exe"
+#     # & $env:USERPROFILE/.spacemacs.d/scripts/e.bat
+# }
