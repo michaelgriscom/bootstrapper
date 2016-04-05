3f0038983ac469a528df16efece3f899d113b0f3
Mike Lim <mike@mjlim.net>
2016-04-04 21:05:43 -0700

    Switch themes around, set default theme to 'monokai'

--- init.el
+++ init.el
@@ -124,15 +124,13 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(zenburn
-                         monokai
-                         spacemacs-dark
+   dotspacemacs-themes '(monokai
                          spacemacs-light
+                         zenburn
+                         spacemacs-dark
                          solarized-light
                          solarized-dark
-                         leuven
-                         monokai
-                         zenburn)
+                         leuven)
    ;; If non nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font. `powerline-scale' allows to quickly tweak the mode-line
