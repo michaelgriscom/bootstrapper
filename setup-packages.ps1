ba7f05281662845f18e8a4aed88d0b0fbb886489
Michael Griscom <migris@microsoft.com>
2018-08-31 10:17:51 +0200

    support for flags

--- setup-packages.ps1
+++ setup-packages.ps1
@@ -6,31 +6,26 @@
 
 $PACKAGES_PATH = "$PSScriptRoot\packages.csv"
 
-function Load-Packages()
-{
-    if (Test-Path $PACKAGES_PATH)
-    {
+function Load-Packages() {
+    if (Test-Path $PACKAGES_PATH) {
         $script:packages = Get-Content $PACKAGES_PATH | ConvertFrom-Csv |
-          select packagename, params
+            select packagename, params, flags
     }
-    else
-    {
+    else {
         echo "Package csv doesn't exist, this is a fatal error!"
         Exit 1
     }
 }
 
-function Install-Choco-Package($package)
-{
-    choco upgrade -y $package.packagename -params $package.params --allowEmptyChecksums --limit-output
+function Install-Choco-Package($package) {
+    echo $package.flags
+    choco upgrade -y $package.packagename -params $package.params --allowEmptyChecksums --limit-output $package.flags
     Run-Script-Package $package "-onInstall"
 }
 
-function Run-Script-Package($package, $suffix = "")
-{
+function Run-Script-Package($package, $suffix = "") {
     $scriptPath = "$PSScriptRoot\scriptPackages\$($package.packagename)$suffix.ps1"
-    if (Test-Path $scriptPath)
-    {
+    if (Test-Path $scriptPath) {
         echo "Running: $scriptPath"
         . $scriptPath
     }
@@ -44,25 +39,21 @@ git pull
 $gitExitCode = $LASTEXITCODE
 popd
 
-if ($gitExitCode -ne 0)
-{
+if ($gitExitCode -ne 0) {
     Save-State
     echo "Git encountered an error, please resolve before updating again."
     Exit 1
 }
 
-ForEach ($package in $script:packages)
-{
+ForEach ($package in $script:packages) {
     echo "Installing/updating package: $($package.packagename)"
-    if ($package.packagename -match '\.ps1')
-    {
+    if ($package.packagename -match '\.ps1') {
         echo "Executing: $($package.packagename)"
         # replace packagename
         $package.packagename = ($package.packagename -replace '\.ps1', '')
         Run-Script-Package $package
     }
-    else
-    {
+    else {
         Install-Choco-Package $package
     }
 }
