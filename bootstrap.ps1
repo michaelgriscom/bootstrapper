431eb90ed9c0763a0b0a3e1e2e73bfe07fc3c758
Michael Griscom <migris@microsoft.com>
2019-03-01 08:53:24 +0100

    automatically elevate permissions

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,4 +1,22 @@
-#Requires -RunAsAdministrator
+function Verify-Elevated {
+    # Get the ID and security principal of the current user account
+    $myIdentity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $myPrincipal = new-object System.Security.Principal.WindowsPrincipal($myIdentity)
+    # Check to see if we are currently running "as Administrator"
+    return $myPrincipal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
+}
+
+# Check to see if we are currently running "as Administrator"
+if (!(Verify-Elevated)) {
+   $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+   $newProcess.Arguments = $myInvocation.MyCommand.Definition;
+   $newProcess.Verb = "runas";
+   [System.Diagnostics.Process]::Start($newProcess);
+
+   exit
+}
+
+exit
 
 if (!(Get-Command "choco.exe" -ErrorAction SilentlyContinue)) {
     Write-Host "Installing Chocolatey"
