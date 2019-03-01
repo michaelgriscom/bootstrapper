e71602baad7284e90c486be59152cdd6f895f892
Michael Griscom <migris@microsoft.com>
2019-03-01 09:15:45 +0100

    separate reg keys into csv

--- configure-explorer.ps1
+++ configure-explorer.ps1
@@ -1,4 +1,6 @@
-﻿# Show Task Manager details
+﻿#Requires -RunAsAdministrator
+
+# Show Task Manager details
 Function ShowTaskManagerDetails {
     Write-Host "Showing task manager details..."
     If (!(Test-Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\TaskManager")) {
@@ -19,56 +21,26 @@ Function ShowTaskManagerDetails {
     Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\TaskManager" -Name "Preferences" -Type Binary -Value $preferences.Preferences
 }
 
-Write-Host "Enabling Remote Desktop w/o Network Level Authentication..."
-Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Type DWord -Value 0
-Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" -Name "UserAuthentication" -Type DWord -Value 0
-
-Write-Host "Remove Edge desktop shortcut"
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" -Name "DisableEdgeDesktopShortcutCreation" -Type DWord -Value 1
-
-Write-Host "Showing file operations details..."
-If (!(Test-Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\OperationStatusManager")) {
-    New-Item -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\OperationStatusManager" | Out-Null
-}
-Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\OperationStatusManager" -Name "EnthusiastMode" -Type DWord -Value 1
-
-Write-Host "Show known file extensions"
-Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "HideFileExt" -Type DWord -Value 0
-
-Write-Host "Show hidden files"
-Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "Hidden" -Type DWord -Value 1
-
-Write-Host "Changing default Explorer view to This PC"
-Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "LaunchTo" -Type DWord -Value 1
-
-Write-Host "Disabling shaking"
-Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "DisallowShaking" -Type DWord -Value 1
-
-Write-Host "Disabling quick edit in console"
-Set-ItemProperty -Path "HKCU:\Console" -Name "QuickEdit" -Type DWord -Value 0
-
-Write-Host "Avoid creating Thumbs.db files on network volumes"
-Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" "DisableThumbnailsOnNetworkFolders" 1
-
-# task bar
-Write-Host "Configuring TaskBar..."
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name "SearchboxTaskbar" -Type DWord -Value 0
-
-Write-Host "Don't show Windows Store Apps on Taskbar"
-Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" "StoreAppsOnTaskbar" 0
-
-# hide taskview
-If (!(Test-Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\MultiTaskingView")) {
-    New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\MultiTaskingView" | Out-Null
+$REG_KEYS_PATH = "$PSScriptRoot\reg-keys.csv"
+function Load-Keys() {
+    if (Test-Path $REG_KEYS_PATH) {
+        $script:keys = Get-Content $REG_KEYS_PATH | ConvertFrom-Csv |
+            select path, name, type, value, description
+    }
+    else {
+        Write-Error "Reg key csv doesn't exist, this is a fatal error!"
+        Exit 1
+    }
 }
 
-If (!(Test-Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\MultiTaskingView\AllUpView")) {
-    New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\MultiTaskingView\AllUpView" | Out-Null
+function Set-Reg-Key($key) {
+    echo $key.description
+    Set-ItemProperty -Path $key.path -Name $key.name -Type $key.type -Value $key.value
 }
 
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\MultiTaskingView\AllUpView" -Name "Enabled" -Type DWord -Value 0
-
-# hide people button
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People" -Name "PeopleBand" -Type DWord -Value 0
+Load-Keys
+ForEach ($key in $script:keys) {
+    Set-Reg-Key $key
+}
 
 ShowTaskManagerDetails
